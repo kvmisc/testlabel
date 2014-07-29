44238cb0ab5b3f6c4e26f77c5224f3234a8c4e7b
wuhp0415 <wuhp0415@thundersoft.com>
2014-07-29 17:30:33 +0800

    blah blah

---
+++ LabelDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LabelDemo
+//
+//  Created by Kevin Wu on 7/29/14.
+//  Copyright (c) 2014 Tapmob. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
