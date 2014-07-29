38680e48b25d2de297c6768a9e131299da549e5d
Kevin Wu <kvmisc@163.com>
2014-07-29 17:50:12 +0800

    blah blah

--- LabelDemo/AppDelegate.h
+++ LabelDemo/AppDelegate.h
@@ -8,8 +8,12 @@
 
 #import <UIKit/UIKit.h>
 
-@interface AppDelegate : UIResponder <UIApplicationDelegate>
+@interface AppDelegate : UIResponder<
+    UIApplicationDelegate
+> {
+  UIWindow *_window;
+}
 
-@property (strong, nonatomic) UIWindow *window;
+@property (nonatomic, strong) UIWindow *window;
 
 @end
---
+++ Podfile
@@ -0,0 +1,2 @@
+platform :ios, "6.0"
+pod 'TTTAttributedLabel', '~> 1.10.0'
